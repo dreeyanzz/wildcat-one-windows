13534f1299932ca56fed423c8702c11ac58c84e0
Adrian Seth Tabotabo <adriangwapoz125@gmail.com>
2026-02-25 15:20:00 +0800

    removed hub

--- hub/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/wildcat-one-windows/', // Base path for GitHub Pages
-})
